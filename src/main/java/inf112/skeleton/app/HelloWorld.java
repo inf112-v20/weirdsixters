37b7d0ec552f7b76dcd0d568f9fffa2650afa5be
zargot <zargot.ripley@gmail.com>
2020-02-06 17:17:56 +0100

    add player movement

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -2,6 +2,8 @@ package inf112.skeleton.app;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
@@ -17,7 +19,7 @@ import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.gdx.math.Vector2;
 
-public class HelloWorld implements ApplicationListener {
+public class HelloWorld extends InputAdapter implements ApplicationListener {
     private static final float MAP_SIZE_X = 5;
     private static final float MAP_SIZE_Y = 5;
     private SpriteBatch batch;
@@ -29,7 +31,6 @@ public class HelloWorld implements ApplicationListener {
     private TiledMapTileLayer boardLayer, playerLayer, holeLayer, flagLayer;
     private TiledMapTileLayer.Cell playerWonCell, playerDiedCell, playerCell;
     private Vector2 playerPos;
-
 
     @Override
     public void create() {
@@ -57,6 +58,7 @@ public class HelloWorld implements ApplicationListener {
         playerWonCell = new TiledMapTileLayer.Cell().setTile(new StaticTiledMapTile(playerTextureSplit[0][2]));
         playerPos = new Vector2(0,0);
 
+        Gdx.input.setInputProcessor(this);
     }
 
     @Override
@@ -67,11 +69,28 @@ public class HelloWorld implements ApplicationListener {
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 1, 1, 1);
-        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+        clearFramebuffer();
+
+        // I'm not gonna read the player pos from the "presentation layer".
+        // This is a one way street.
+        // We're assigning all player positions each frame, for now.
+        clearCells(playerLayer);
+        playerLayer.setCell((int) playerPos.x,(int) playerPos.y, playerCell);
 
         renderer.render();
-        playerLayer.setCell((int) playerPos.x,(int) playerPos.y,playerCell);
+    }
+
+    private void clearFramebuffer(){
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+    }
+
+    private void clearCells(TiledMapTileLayer layer){
+        for (int y = 0; y < MAP_SIZE_Y; y++){
+            for (int x = 0; x < MAP_SIZE_X; x++){
+                layer.setCell(x, y, null);
+            }
+        }
     }
 
     @Override
@@ -84,5 +103,30 @@ public class HelloWorld implements ApplicationListener {
 
     @Override
     public void resume() {
+    }
+
+    /*
+     * @keyUp makes it feel laggy, we're using @keyDown for immediate response.
+     * This won't be a problem because it's not triggered on pressed keys.
+     */
+    @Override
+    public boolean keyDown(int key) {
+        int x = 0, y = 0;
+        switch (key){
+            case Input.Keys.RIGHT: x++; break;
+            case Input.Keys.LEFT: x--; break;
+            case Input.Keys.UP: y++; break;
+            case Input.Keys.DOWN: y--; break;
+        }
+        movePlayer(x, y);
+        return true;
+    }
+
+    /*
+     * I'm not checking if the player goes outside of the board
+     * because it's a bad idea to write code before we know it's needed.
+     */
+    private void movePlayer(int x, int y){
+        playerPos.add((float)x, (float)y);
     }
 }
