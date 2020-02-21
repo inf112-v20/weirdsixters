9c08ad4936c91400905f741e29127c6ab3e99fe1
zargot <zargot.ripley@gmail.com>
2020-02-21 18:01:48 +0100

    tile: work on import and methods

--- src/main/java/inf112/skeleton/app/Tile.java
+++ src/main/java/inf112/skeleton/app/Tile.java
@@ -1,20 +1,96 @@
 package inf112.skeleton.app;
 
+import com.badlogic.gdx.math.Vector2;
+
 enum TileKind {
-    empty, wall, hole, belt, gear, flag, wrench
+    empty, belt, flag, gear, hole, laserWall, wall, wrench
 }
 
 public class Tile {
-    private final int value;
-    private final TileKind kind;
 
-    public Tile(TileKind kind) {
-        this.kind = kind;
-        this.value = 0;
+    // one bit per direction
+    private class DirectionBit {
+        public static final int RIGHT = 0b0001;
+        public static final int UP    = 0b0010;
+        public static final int LEFT  = 0b0100;
+        public static final int DOWN  = 0b1000;
     }
 
-    public Tile(TileKind kind, int value) {
+    // right-angled walls
+    private final static int CORNER_MASKS[] = {
+            (DirectionBit.RIGHT & DirectionBit.UP),
+            (DirectionBit.UP    & DirectionBit.LEFT),
+            (DirectionBit.LEFT  & DirectionBit.DOWN),
+            (DirectionBit.DOWN  & DirectionBit.RIGHT)};
+
+    // all possible wall-direction combinations
+    // matches the order in which tile-values are imported
+    private final static int DIRECTION_MASKS[] = {
+            DirectionBit.RIGHT, DirectionBit.UP, DirectionBit.LEFT, DirectionBit.DOWN,
+            CORNER_MASKS[0], CORNER_MASKS[1], CORNER_MASKS[2], CORNER_MASKS[3]};
+
+    // public fields
+    // guaranteed immutable through "final", no getters needed.
+    public final TileKind kind;
+    public final int value;
+    public final Vector2 position, direction;
+
+    public Tile(TileKind kind, Vector2 position) {
+        this(kind, 0, position, new Vector2(0,0));
+    }
+
+    public Tile(TileKind kind, int value, Vector2 position) {
+        this(kind, value, position, new Vector2(0,0));
+    }
+
+    public Tile(TileKind kind, int value, Vector2 position, Vector2 direction) {
         this.kind = kind;
         this.value = value;
+        this.position = position;
+        this.direction = direction;
+    }
+
+    /**
+     * @summary Gear rotation "direction", etc.
+     * @return -1 or +1
+     */
+    public float sign() {
+        assert(value >= 0 && value <= 1);
+        return (value == 0) ? -1 : 1;
+    }
+
+    /**
+     * @return true if the tile represents two walls/lasers.
+     */
+    public boolean isDouble() {
+        return value > 3;
+    }
+
+    /**
+     * @summary Treats tile-value as a direction and compares @fromDir with
+     * the tile's blocking direction.
+     * @param fromDir is the direction from the caller to the tile
+     * @return true if this tile blocks movement from @fromDir
+     */
+    public boolean isBlocking(Vector2 fromDir) {
+        if (kind != TileKind.wall && kind != TileKind.laserWall)
+            return false;
+        Vector2 toCaller = Linear.neg(fromDir);
+        int toCallerMask = directionMask(toCaller);
+        int blockDirMask = DIRECTION_MASKS[value];
+        return (toCallerMask & blockDirMask) > 0;
+    }
+
+    private int directionMask(Vector2 dir) {
+        assert(Linear.isUnit(dir));
+        if (dir.x == 1)
+            return DirectionBit.RIGHT;
+        if (dir.y == 1)
+            return DirectionBit.UP;
+        if (dir.x == -1)
+            return DirectionBit.LEFT;
+        if (dir.y == -1)
+            return DirectionBit.DOWN;
+        return 0;
     }
 }
--- src/main/java/inf112/skeleton/app/TileUtils.java
+++ src/main/java/inf112/skeleton/app/TileUtils.java
@@ -1,5 +1,6 @@
 package inf112.skeleton.app;
 
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.math.Vector2;
 import org.apache.commons.lang3.ArrayUtils;
@@ -9,6 +10,8 @@ public class TileUtils {
     // right, up, left, down
     // low, high
     private static final int holeId = 5;
+    private static final int[] gearIds = {52, 53};
+    private static final int[] laserWallIds = {45, 44, 37, 36, 94, 93, 92, 86};
     private static final int[] wallIds = {22, 30, 29, 28, 15, 23, 31, 7};
     private static final int[] wrenchIds = {14, 6};
 
@@ -17,30 +20,42 @@ public class TileUtils {
         Tile[][] result = new Tile[(int)layerSize.y][(int)layerSize.x];
         for (int y = 0; y < layerSize.y; y++) {
             for (int x = 0; x < layerSize.x; x++) {
-                TiledMapTileLayer.Cell cell = layer.getCell(x, y);
-                if (cell == null)
-                    result[y][x] = createEmptyTile();
-                int id = cell.getTile().getId();
-                result[y][x] = createTile(id);
+                TiledMapTile tile = layer.getCell(x, y).getTile();
+                Vector2 position = new Vector2(x, y);
+                if (tile == null)
+                    result[y][x] = createEmptyTile(position);
+                result[y][x] = createTile(position, tile.getId());
             }
         }
         return result;
     }
 
-    private static Tile createEmptyTile() {
-        return new Tile(TileKind.empty, 0);
+    private static Tile createEmptyTile(Vector2 position) {
+        return new Tile(TileKind.empty, position);
     }
 
-    private static Tile createTile(int id) {
+    // constructs Tile from tile id (spritesheet index)
+    private static Tile createTile(Vector2 position, int id) {
         int ix;
         if (id == holeId)
-            return new Tile(TileKind.hole);
+            return new Tile(TileKind.hole, position);
+
         ix = ArrayUtils.indexOf(wrenchIds, id);
         if (ix > 0)
-            return new Tile(TileKind.wrench, ix);
+            return new Tile(TileKind.wrench, ix, position);
+
+        ix = ArrayUtils.indexOf(gearIds, id);
+        if (ix > 0)
+            return new Tile(TileKind.gear, ix, position);
+
         ix = ArrayUtils.indexOf(wallIds, id);
         if (ix > 0)
-            return new Tile(TileKind.wall, ix);
-        return createEmptyTile();
+            return new Tile(TileKind.wall, ix, position);
+
+        ix = ArrayUtils.indexOf(laserWallIds, id);
+        if (ix > 0)
+            return new Tile(TileKind.laserWall, ix, position);
+
+        return createEmptyTile(position);
     }
 }
