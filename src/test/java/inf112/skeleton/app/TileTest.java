8a7eeeffba57d8a29713bb6e5b98e326ebcdc11a
Sebastian Cornelius Bergh <Sebastian.Bergh@student.uib.no>
2020-02-25 13:09:06 +0100

    add blocksDir test for empty tile

--- src/test/java/inf112/skeleton/app/TileTest.java
+++ src/test/java/inf112/skeleton/app/TileTest.java
@@ -1,13 +1,18 @@
 package inf112.skeleton.app;
 
+import com.badlogic.gdx.math.Vector2;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class TileTest {
     @Test
-    public void blocksDirTest() {
-        assertTrue(false);
+    public void blocksDir_empty_notBlocking() {
+        Tile tile = new Tile(TileKind.empty);
+        for (Direction enumDir : Direction.values()) {
+            Vector2 dir = enumDir.toVector2();
+            assertEquals(false, tile.blocksDir(dir, false));
+            assertEquals(false, tile.blocksDir(dir, true));
+        }
     }
-
 }
