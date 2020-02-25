67f209116024816782caf123a288d8b8e9b5cda2
Frank Rune Espeseth <Frank.Espeseth@student.uib.no>
2020-02-25 13:15:58 +0100

    Lag en feilende test for Linear.add

---
+++ src/test/java/inf112/skeleton/app/LinearTest.java
@@ -0,0 +1,13 @@
+package inf112.skeleton.app;
+
+import com.badlogic.gdx.math.Vector2;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LinearTest {
+    @Test
+    public void addTest(){
+        assertEquals(new Vector2(0,2), Linear.add(new Vector2(0,0), new Vector2(0,1)));
+    }
+}
