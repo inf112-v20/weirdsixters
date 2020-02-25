2aaad5ee3cb973cb5912a837052c19f179a2eb5a
Sebastian Cornelius Bergh <Sebastian.Bergh@student.uib.no>
2020-02-25 12:59:30 +0100

    Add failing blocksDirTest

---
+++ src/test/java/inf112/skeleton/app/TileTest.java
@@ -0,0 +1,13 @@
+package inf112.skeleton.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TileTest {
+    @Test
+    public void blocksDirTest() {
+        assertTrue(false);
+    }
+
+}
