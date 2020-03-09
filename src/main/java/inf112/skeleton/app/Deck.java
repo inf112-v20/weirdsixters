871e44302cb8543de2e5f473d5e43fdb33688e02
zargot <zargot.ripley@gmail.com>
2020-03-09 16:40:38 +0100

    deck: add drawCard, discardCard

--- src/main/java/inf112/skeleton/app/Deck.java
+++ src/main/java/inf112/skeleton/app/Deck.java
@@ -1,11 +1,33 @@
 package inf112.skeleton.app;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
     private ArrayList<Card> stack, discarded;
 
     public Deck() {
+        stack = new ArrayList<>(Card.programCards);
+        Collections.shuffle(stack);
+    }
 
+    public Card drawCard() {
+        if (stack.isEmpty()) {
+            assert(!discarded.isEmpty());
+            swapStacks();
+            Collections.shuffle(stack);
+        }
+        return ListUtils.pop(stack);
+    }
+
+    public void discardCard(Card card) {
+        assert(!stack.contains(card));
+        discarded.add(card);
+    }
+
+    private void swapStacks() {
+        ArrayList<Card> tmp = stack;
+        stack = discarded;
+        discarded = tmp;
     }
 }
