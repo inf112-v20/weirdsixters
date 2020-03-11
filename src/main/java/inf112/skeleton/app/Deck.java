3ecc0e7ded0b8e9c10a3db2fe14448c3dfe4974e
zargot <zargot.ripley@gmail.com>
2020-03-11 12:32:26 +0100

    deck: refactor duplicate code

--- src/main/java/inf112/skeleton/app/Deck.java
+++ src/main/java/inf112/skeleton/app/Deck.java
@@ -9,17 +9,19 @@ public class Deck {
 
     public Deck(ArrayList<Card> cards) {
         this.cards = new ArrayList(cards);
-        head = this.cards.size();
-        Collections.shuffle(this.cards);
+        reset();
     }
 
     public Card drawCard() {
         assert(head >= 0 && head <= cards.size());
-        if (head == 0) {
-            head = cards.size();
-            Collections.shuffle(cards);
-        }
+        if (head == 0)
+            reset();
         assert(head > 0);
         return cards.remove(--head);
+    }
+
+    private void reset() {
+        head = cards.size();
+        Collections.shuffle(cards);
     }
 }
