ac1d447a3192edade25bac4ad914eaf3bc0ad11a
Frank Rune Espeseth <Frank.Espeseth@student.uib.no>
2020-03-25 18:35:19 +0100

    Done renaming LinearTest. Made one test instead of many

--- src/test/java/inf112/skeleton/app/LinearTest.java
+++ src/test/java/inf112/skeleton/app/LinearTest.java
@@ -8,45 +8,16 @@ import static org.junit.Assert.assertEquals;
 
 public class LinearTest {
     @Test
-    public void addTest(){
+    public void Linear_someNumbers_correctResult(){
         assertEquals(new Vector2(0,1), Linear.add(new Vector2(0,0), new Vector2(0,1)));
-    }
-    @Test
-    public void subTest(){
         assertEquals(new Vector2(0,-2), Linear.add(new Vector2(0,-1), new Vector2(0,-1)));
-    }
-    @Test
-    public void mulTest(){
         assertEquals(new Vector2(3,-8), Linear.mul(new Vector2(1,2), new Vector2(3,-4)));
-    }
-    @Test
-    public void divTest(){
         assertEquals(new Vector2(1,1), Linear.div(new Vector2(4,1),new Vector2(4,1)));
-    }
-    @Test
-    public void minTest(){
         assertEquals(5, Linear.min(new Vector2(5,10)), 0.001);
-    }
-    @Test
-    public void maxTest(){
         assertEquals(10, Linear.max(new Vector2(5,10)), 0);
-    }
-    @Test
-    public void sclTest(){
         assertEquals(new Vector2(6,3), Linear.scl(new Vector2(2,1), (3)));
-    }
-    @Test
-    public void negTest(){
         assertEquals(new Vector2(-2,-2), Linear.neg(new Vector2(2,2)));
-    }
-    @Test
-    public void floorTest(){
         assertEquals(new Vector2(0,0), Linear.floor(new Vector2(0.5f, 0.5f)));
-    }
-    @Test
-    public void roundTest(){
         assertEquals(new Vector2(1,1), Linear.round(new Vector2(0.9f,0.8f)));
     }
-
-
 }
