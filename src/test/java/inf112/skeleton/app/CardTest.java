d2f8ac230673e56ae0627efb463ae6f0214e9926
Frank Rune Espeseth <Frank.Espeseth@student.uib.no>
2020-03-25 18:57:43 +0100

    Finally done renaming CardTest

--- src/test/java/inf112/skeleton/app/CardTest.java
+++ src/test/java/inf112/skeleton/app/CardTest.java
@@ -8,21 +8,12 @@ import static org.junit.Assert.assertEquals;
 public class CardTest {
 
     @Test
-    public void card_forward_fowardValue(){
+    public void card_forward_fowardValue() {
         Card card1 = new Card(FORWARD, 2, 12);
         assertEquals(FORWARD, card1.kind);
-    }
-
-    @Test
-    public void card_two_twoSteps(){
-        Card card1 = new Card(FORWARD, 2, 12);
         assertEquals(2, card1.steps);
-    }
-
-    @Test
-    public void card_twelve_priorityTwelve(){
-        Card card1 = new Card(FORWARD, 2, 12);
         assertEquals(12, card1.priority);
+
     }
 }
 
