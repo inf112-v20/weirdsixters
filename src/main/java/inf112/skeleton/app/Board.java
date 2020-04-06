7b5557d691bae9e0611f1428c5a74ca74471de65
Frank Rune Espeseth <Frank.Espeseth@student.uib.no>
2020-04-06 16:01:08 +0200

    Endret robotGrid fra public->private

--- src/main/java/inf112/skeleton/app/Board.java
+++ src/main/java/inf112/skeleton/app/Board.java
@@ -3,7 +3,7 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.math.Vector2;
 
 public class Board {
-    public Robot[][] robotGrid;
+    private Robot[][] robotGrid;
 
     private Tile[][] grid;
 
